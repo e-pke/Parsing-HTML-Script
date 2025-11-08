b294bc8a1cf21e5bf33ee35753b9c9f4d20ac350
€pke <gujerke@gmail.com>
2025-11-08 13:10:35 -0300

    Update parsing.sh

--- parsing.sh
+++ parsing.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-if [ "$1" == "" 2> null ]
+if [ "$1" == "" 2>/dev/null ]
 then
-	echo -e "\033[1;31m   >>>>> USO CORRETO: ./parsing.sh www.site.com"
+	echo -e "\033[1;31m   >>>>> USO CORRETO: ./parsing.sh <url>"
 else
-	echo -e "\033[1;32m   >>>>> ACESSANDO SITE"
+	echo -e "\033[1;32m   >>>>> ACESSANDO PÁGINA"
 	echo -e "\033[1;37m "
-	wget $1 2> null
+	wget $1 2>/dev/null
 	echo -e "\033[1;34m   >>>>> BUSCANDO HOSTS NA PÁGINA" "\033[1;37m "
 	grep href index.html | grep http | cut -d "/" -f3 | cut -d '"' -f1  | grep "\." | grep -v " "> lista
 	cat lista
@@ -18,16 +18,15 @@ else
 	echo -e "\033[1;37m "
 	echo -e "\033[1;33m   >>>>> PRONTO!" "\033[1;32m "
 	echo " "
-	rm null
 	rm hosts
 	rm index.html
 	rm lista
-	read -p "   >>>>> DESEJA FAZER OUTRO? [y/n] " answer
+	read -p "   >>>>> DESEJA FAZER NOVAMENTE? [y/n] " answer
 	if [ "$answer" != "y" ]
 	then
 		echo -e "\033[1;31m   >>>>> ENCERRADO!" "\033[1;37m "
 	else
-		read -p "   >>>>> DIGITE O ENDEREÇO DO NOVO SITE:  " alvo
+		read -p "   >>>>> DIGITE A NOVA URL:  " alvo
 		echo " "
 		./$0 $alvo
 	fi
